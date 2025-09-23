6919c2c02f07e35f74a9832420c11b938658daa4
Vid-ya-386 <2420030386@klh.edu.in>
2025-09-23 22:03:13 +0530

    All week folders in this branch

--- Week7/Week7/data.js
+++ Week7/Week7/data.js
@@ -1,6 +1,11 @@
 export const books = [
-{ title: "A good girl's guide to murder", author: "Holly Jackson", price: 499, availability: "in stock" },
-{ title: "The Housemaid", author: "Frieda McFadden", price: 699, availability: "in stock" },
-{ title: "One of us is lying", author: "Mark Lee", price: 599, availability: "out of stock" },
-{ title: "Python for Everyone", author: "Ramya Krishna", price: 450, availability: "in stock" }
+  { title: "One of Us Is Lying", author: "Karen M. McManus", price: 399, availability: "in stock" },
+  { title: "One of Us Is Next", author: "Karen M. McManus", price: 420, availability: "in stock" },
+  { title: "One of Us Is Back", author: "Karen M. McManus", price: 450, availability: "in stock" },
+  { title: "The Housemaid", author: "Freida McFadden", price: 499, availability: "in stock" },
+  { title: "The Housemaid's Secret / Watching", author: "Freida McFadden", price: 520, availability: "in stock" },
+  { title: "A Good Girl's Guide to Murder", author: "Holly Jackson", price: 399, availability: "in stock" },
+  { title: "As Good As Dead", author: "Holly Jackson", price: 420, availability: "in stock" },
+  { title: "I Fell in Love with Hope", author: "Lancali", price: 450, availability: "in stock" },
+  { title: "Twisted Games", author: "Ana Huang", price: 499, availability: "in stock" }
 ];
