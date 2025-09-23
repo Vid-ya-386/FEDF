ae525ca23b33bcdab7c76bbf714ae1562ac1c999
Vid-ya-386 <2420030386@klh.edu.in>
2025-09-23 19:12:19 +0530

    Initial commit

---
+++ Week7/Week7/data.js
@@ -0,0 +1,6 @@
+export const books = [
+{ title: "A good girl's guide to murder", author: "Holly Jackson", price: 499, availability: "in stock" },
+{ title: "The Housemaid", author: "Frieda McFadden", price: 699, availability: "in stock" },
+{ title: "One of us is lying", author: "Mark Lee", price: 599, availability: "out of stock" },
+{ title: "Python for Everyone", author: "Ramya Krishna", price: 450, availability: "in stock" }
+];
